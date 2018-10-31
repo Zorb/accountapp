627debc810d04b3688c998f0175e659dc9982fc2
Zorb <al3xzorbas@gmail.com>
2018-10-31 16:52:58 +0000

    sql

---
+++ accountapp/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (1, 'dasdsad', 'asdasdasd', 'asdasdasd')
+INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (2, 'asdsadasdasd', 'genre2', 'asdasdasdasd')
+INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (3, 'asdasdasdasd', 'asdasdasdasd', 'asdasdasdas')
